2de05e99573b9403bc4eda34e37bc9ef5140a9c4
UlfhednarKutral <martinfrobertsweb@gmail.com>
2021-02-03 22:35:16 -0300

    carrilfix

--- src/index/components/Carril.vue
+++ src/index/components/Carril.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed mx-auto md:py-11 md:px-11 bg-white" style="height: 100vh; width:100vw;">
+    <div class="fixed mx-auto py-4 px-4 bg-white" style="height: 100vh; width:100vw;">
       <div class=" mx-w-7xl  border-t-2 border-l-2 border-solid border-blue" style="height: 70vh;" >
       </div>
     </div>
