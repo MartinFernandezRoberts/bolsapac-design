572ad4481c6856ad4e39a4851cf0f2c0ea92645f
UlfhednarKutral <martinfrobertsweb@gmail.com>
2021-04-21 21:09:45 -0400

    Active en productos, falta en nav

--- src/index/components/nav/Elementos.vue
+++ src/index/components/nav/Elementos.vue
@@ -2,22 +2,22 @@
     
 
   <div class="relative flex items-baseline space-x-16 ml-20">
-    <router-link to="/somos" class="px-2 py-2 rounded-md" >
-      <p class="text-sm font-mukta w600 text-gray-600 lg:text-base hover:text-black">
+    <router-link :to="{ name: 'somos'}" class="px-2 py-2 rounded-md" active-class="text-black"  >
+      <p class="text-sm font-mukta w600 text-gray-600 lg:text-base hover:text-black" >
       SOMOS
       </p>
     </router-link>
-    <router-link to="/sustentabilidad" class="px-3 py-2 rounded-md" >
+    <router-link :to="{ name: 'sustentabilidad'}" class="px-3 py-2 rounded-md" active-class="text-black" >
       <p class="text-sm font-mukta w600 text-gray-600 lg:text-base hover:text-black">
       SUSTENTABILIDAD
       </p>
     </router-link>
-    <router-link to="/procesos" class="px-3 py-2 rounded-md" >
+    <router-link :to="{ name: 'procesos'}" class="px-3 py-2 rounded-md" active-class="text-black" >
       <p class="text-sm font-mukta w600 text-gray-600 lg:text-base hover:text-black">
       PROCESOS
       </p>
     </router-link>
-    <router-link to="/contacto" class="px-3 py-2 rounded-md" >
+    <router-link :to="{ name: 'contacto'}" class="px-3 py-2 rounded-md" active-class="text-black" >
       <p class="text-sm font-mukta w600 text-gray-600 lg:text-base hover:text-black">
       CONTACTO
       </p>
@@ -29,7 +29,12 @@
 
 <script>
 export default {
-    name: 'Elementos'
+  name: 'Elementos',
+  data() {    
+  return {
+    isActive: true
+    }
+  }
 }
 </script>
 
